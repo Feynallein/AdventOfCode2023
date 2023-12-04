de3cc4a330278c990947043715b8378caaf25a9e
Martin Berthier <martin.berthier.mb@gmail.com>
2023-12-04 13:18:44 +0100

    day four

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ class Program {
 	public static void Main(String[] args) {
 		// _ = new DayOne();
 		// _ = new DayTwo();
-		_ = new DayThree();
+		// _ = new DayThree();
+		_ = new DayFour();
 	}
 }
